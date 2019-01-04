f24b77a15cee07efab0b5e3b37c87a88fdec415c
LeeDaeHee <ldh3521@naver.com>
2019-01-04 11:17:41 +0900

    daehee textview add

--- app/src/main/java/misemise/com/gittest/MainActivity.java
+++ app/src/main/java/misemise/com/gittest/MainActivity.java
@@ -3,16 +3,19 @@ package misemise.com.gittest;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         button = (Button)findViewById(R.id.button);
+        textView = (TextView)findViewById(R.id.textView);
 
     }
 }
